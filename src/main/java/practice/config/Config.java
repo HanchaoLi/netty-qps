2b5c3bc283d14bdfc0a2f9c84b7408383bd8de63
hanchao.li <hanchao.li@borngroup.com>
2019-06-05 17:18:11 -0400

    QPS-002: implement client side

--- src/main/java/practice/config/Config.java
+++ src/main/java/practice/config/Config.java
@@ -1,6 +1,7 @@
-package practice.server;
+package practice.config;
 
 public class Config {
     public static final int BEGIN_PORT = 8000;
     public static final int END_PORT = 8050;
+    public static final String SERVER_IP = "127.0.0.1";
 }
--- src/main/java/practice/server/NettyServer.java
+++ src/main/java/practice/server/NettyServer.java
@@ -6,6 +6,7 @@ import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import practice.config.Config;
 
 public class NettyServer {
 
