ca974d99543911abd60d8226e7e1323bce2ea306
hanchao.li <hanchao.li@borngroup.com>
2019-06-05 16:39:25 -0400

    init server side code

---
+++ src/main/java/practice/server/Config.java
@@ -0,0 +1,22 @@
+package practice.server;
+
+public class Config {
+    private int beginPort;
+    private int endPort;
+
+    public int getBeginPort() {
+        return beginPort;
+    }
+
+    public void setBeginPort(int beginPort) {
+        this.beginPort = beginPort;
+    }
+
+    public int getEndPort() {
+        return endPort;
+    }
+
+    public void setEndPort(int endPort) {
+        this.endPort = endPort;
+    }
+}
---
+++ src/main/java/practice/server/NettyServer.java
@@ -0,0 +1,40 @@
+package practice.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class NettyServer {
+
+    public static void main(String[] args) {
+
+    }
+    public void run(int beginPort, int endPort) {
+        System.out.println("server starting...!");
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bossGroup, workGroup)
+                .channel(NioServerSocketChannel.class)
+                .childOption(ChannelOption.SO_BACKLOG, 1024)
+                .childOption(ChannelOption.SO_REUSEADDR, true);
+
+        serverBootstrap.childHandler(new TcpCountHandler());
+
+        for (; beginPort < endPort;beginPort++) {
+            int port = beginPort;
+            serverBootstrap.bind(port).addListener((ChannelFutureListener) future -> {
+                System.out.println("server successfully bind port : " + port);
+            });
+        }
+
+
+        System.out.println("Echo initlize");
+
+    }
+}
