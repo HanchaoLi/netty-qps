688fd6b8f84842fd8fd4e040222c069cc40596e5
HanchaoLi <seraphme1234@gmail.com>
2020-01-21 07:50:12 -0500

    Update NettyServer.java

--- src/main/java/practice/server/NettyServer.java
+++ src/main/java/practice/server/NettyServer.java
@@ -3,7 +3,7 @@ package practice.server;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelOption;
-import io.netty.channel. EventLoopGroup;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import practice.config.Config;
